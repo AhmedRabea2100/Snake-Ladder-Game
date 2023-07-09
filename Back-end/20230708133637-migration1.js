d01d35277a6604c35b979a4a57e41128ddc81fd4
yarayehiaa <106600476+yarayehiaa@users.noreply.github.com>
2023-07-09 10:10:10 +0300

    Add files via upload

---
+++ Back-end/20230708133637-migration1.js
@@ -0,0 +1,207 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Status', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      value: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+
+    // Create Board table
+    await queryInterface.createTable('Board', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+
+    // Create Game table
+    await queryInterface.createTable('Game', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      statusId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Status',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      numberOfPlayers: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      currentPlayerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      boardId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Board',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+    await queryInterface.createTable('BoardElement', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      from: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      to: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      boardId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Board',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+    
+    // Create PlayerGame table
+    await queryInterface.createTable('PlayerGame', {
+      playerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Player',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      gameId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Game',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      position: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+    
+    // Create Player table
+    await queryInterface.createTable('Player', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.dropTable('Player');
+   
+    await queryInterface.dropTable('PlayerGame');
+
+    // Drop BoardElement table
+    await queryInterface.dropTable('BoardElement');
+  
+    // Drop Game table
+    await queryInterface.dropTable('Game');
+  
+    // Drop Board table
+    await queryInterface.dropTable('Board');
+  
+    // Drop Status table
+    await queryInterface.dropTable('Status');
+  
+    // Drop Player table
+    
+  
+
+  }
+};
